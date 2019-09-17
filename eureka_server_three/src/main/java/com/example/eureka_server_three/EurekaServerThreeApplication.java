35f2e70adf8f3bb2629831c838c84e50e06d116f
wesley <tgfc040@tgfc.tw>
2019-09-17 16:45:24 +0800

    three

---
+++ eureka_server_three/src/main/java/com/example/eureka_server_three/EurekaServerThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_server_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerThreeApplication.class, args);
+    }
+
+}
